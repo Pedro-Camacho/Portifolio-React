53e5bdf6fd783cbfcaba31550b8609ce7a19bd26
Pedro Almeida e Camacho <49591140+Pedro-Camacho@users.noreply.github.com>
2024-10-07 12:59:59 -0300

    Update Principal.jsx

--- src/Paginas/Principal.jsx
+++ src/Paginas/Principal.jsx
@@ -7,7 +7,7 @@ export default function Principal(){
                 <img src={Perfil} className='rounded-padrao'></img>
             </div>
             <div className='flex flex-col pl-12 pt-7 w-[595px] h-[347px] mt-48 bg-gradient-to-br from-containerFade to-containerFade2 rounded-padrao border-r-4 border-b-4 border-sombra'>
-                <h2 className='font-jetBrains text-[48px] text-left text-white'>Ola, eu sou</h2>
+                <h2 className='font-jetBrains text-[48px] text-left text-white'>Olá, eu sou</h2>
                 <h2 className='font-jetBrains text-[48px] text-left mb-11 text-rosa'>Pedro Camacho</h2>
                 <h3 className='font-jetBrains text-[24px] text-left text-white'>Estudante de Engenharia de Software  na FIAP.</h3>
             </div>
